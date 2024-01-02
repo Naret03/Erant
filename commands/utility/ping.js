d3b8602a99ff3cb6ed4422a24c3c1c66f3ad9449
Naret03 <jcts0407@gmail.com>
2024-01-02 15:57:30 +0100

    Finalized support for commands

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('hello_there')
-		.setDescription('such meme'),
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		interaction.reply('General Kenobi');
+		interaction.reply('Pong!');
 	},
 };
