f635bb4cd423f62eaf97740f3b6ecb59a96aa40e
Naret03 <jcts0407@gmail.com>
2024-03-25 11:22:12 +0100

    add-emoji command now adds images. Lacks input sanitising and error handling

--- commands/developer-tools/add-emoji.js
+++ commands/developer-tools/add-emoji.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
+const sharp = require('sharp');
+const maxEmojiSize = 256000;
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,6 +15,11 @@ module.exports = {
 						.setDescription('Image URL')
 						.setRequired(true)
 				)
+				.addStringOption( option => 
+					option.setName('emoji-name')
+						.setDescription('Name of the emoji')
+						.setRequired(true)
+				)
 		)
 		// Subcommand for Image attachment
 		.addSubcommand(subcommand => 
@@ -23,16 +30,65 @@ module.exports = {
 						.setDescription('The image to be added as an emoji')
 						.setRequired(true)
 				)
+				.addStringOption( option => 
+					option.setName('emoji-name')
+						.setDescription('Name of the emoji')
+						.setRequired(true)
+				)
 		),
 	async execute(interaction) {
+		// TODO: GIFs, Sanitizing inputs, handling all the errors
+		let response = 'Response hasn\'t been configured yet...';
+		await interaction.reply(response);
+		// retrieving image URL and emoji name
+		const emojiName = interaction.options.data[0].options[1].value;
 		let URL;
 		if(interaction.options._subcommand == 'attachment'){
 			URL = interaction.options.data[0].options[0].attachment.attachment;
 		}else if(interaction.options._subcommand == 'url'){
 			URL = interaction.options.data[0].options[0].value;
-		}else{
-			URL = 'Error retrieving image';
 		}
-		interaction.reply(URL);
+
+		// Getting the image as a buffer array
+		let bufferArray;
+		await (async () => {
+			try {
+				const response = await fetch(URL);
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
+				bufferArray = await response.arrayBuffer();
+			} catch (error) {
+				console.error(error);
+			}
+		})();
+
+		// Creating the sharp object
+		let image;
+		try {
+			image = sharp(bufferArray).resize(128, 128);
+			let currentSize = (await image.toBuffer()).length;
+			let compressQuality = 90; // Current compression value
+			let compressionStep = 10; // How fast does the comrpession quality drops for each iteration
+			while (currentSize > maxEmojiSize){
+				image.webp({
+					quality: compressQuality, 
+					force: true
+				});
+				compressQuality -= compressionStep;
+				currentSize = (await image.toBuffer()).length;
+				if(compressQuality <= 0){
+					throw new Error('Image couldn\'t be compressed to the right size');
+				}
+			}
+		} catch (error) {
+			console.error(error);
+		}
+
+		// adding the emoji to the server
+		await interaction.guild.emojis.create({
+			name: emojiName,
+			attachment: image
+		});
 	},
 };
